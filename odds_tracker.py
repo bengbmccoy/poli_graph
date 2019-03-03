dfc81efaad7569ca349f2c3bbe29ed153eea61b3
bengbmccoy <benjamingbmccoy@gmail.com>
2019-03-03 13:51:56 +1100

    first push of the odds_tracker, accidentally added the .txt files, will add that to git ignore.

---
+++ odds_tracker.py
@@ -0,0 +1,15 @@
+# sportsbet.py
+
+'''
+Written: Ben McCoy 201903
+
+A script that tracks the betting odds of 2020 US presidential candidates on
+sportsbet.
+
+'''
+
+def main():
+
+    print 'hello world'
+
+main()
